504f7130182f39ad4a54d8f95c61f08960972585
Sone <sthaya@gmail.com>
2020-07-10 08:34:30 -0700

    getting close, not looping and printing right

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,12 +4,21 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
+        
+    def drive():
+        return "vrooom"
 
     # TODO
 
-
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+        self.num_wheels = num_wheels
+        
+    def drive():
+        return "BRAAAP!"
 # Subclass Motorcycle from GroundVehicle.
 #
 # Make it so when you instantiate a Motorcycle, it automatically sets the number
@@ -30,3 +39,9 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+v = [i.drive() for i in vehicles]
+print(v)
+
+
+print(GroundVehicle.drive())
+print(Motorcycle.drive())
